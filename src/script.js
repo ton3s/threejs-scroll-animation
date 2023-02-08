5f356790e1407b01aeaa9427ff003d1c6483cf6c
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-02-07 18:42:25 -0600

    Add magFilter for gradient texture to work as expected

--- src/script.js
+++ src/script.js
@@ -28,6 +28,7 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
 
 // Material
 const material = new THREE.MeshToonMaterial({
